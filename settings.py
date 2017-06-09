117886549b555460a2a644e1c47c7ceb12a97edd
Justin Pan <j.pan1997@gmail.com>
2017-06-09 04:12:29 -0400

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-SPARKPOST_API_KEY = "1b3a8429baf9f5a6b5fb656fa33884004bb76d2b"
-<<<<<<< HEAD
-FIREBASE_API_KEY = "AIzaSyBY4j3iCU0hyp9nIanz5aEAtRK5vgeg_vU"
-=======
-DOMAIN_NAME = "scrapbookit.me"
->>>>>>> origin/master
